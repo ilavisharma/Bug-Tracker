80e3137e40944a1952e4751d477150d6197bcd8c
ilavisharma <lavisharma796@gmail.com>
2020-04-23 01:08:25 +0530

    change to clip loader

--- client/src/utils/LoadingSpinner.js
+++ client/src/utils/LoadingSpinner.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { HashLoader } from 'react-spinners';
+import { ClipLoader } from 'react-spinners';
 import Row from 'react-bootstrap/Row';
 
 const LoadingSpinner = () => (
   <Row className="justify-content-md-center">
-    <HashLoader size={100} color="#2B2B52" />
+    <ClipLoader size={100} color="#2B2B52" />
   </Row>
 );
 
