c3619147ac37bee2b4ab7a596620c2e7ff4f1875
ilavisharma <lavisharma796@gmail.com>
2020-05-15 11:20:40 +0530

    fix: remove react-router-last location

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,6 +1,5 @@
 import React, { useMemo, Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { LastLocationProvider } from 'react-router-last-location';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LandingPage from './Landing/LandingPage';
 import GlobalState from './GlobalState';
@@ -15,17 +14,15 @@ const MemoRouter = () =>
   useMemo(
     () => (
       <BrowserRouter>
-        <LastLocationProvider>
-          <Suspense fallback={<LoadingSpinner />}>
-            <Switch>
-              <Route exact path="/" component={LandingPage} />
-              <Route path="/home" component={Homepage} />
-              <Route exact path="/signin" component={SignIn} />
-              <Route exact path="/signin/demo" component={DemoUserSignIn} />
-              <Route component={Error404} />
-            </Switch>
-          </Suspense>
-        </LastLocationProvider>
+        <Suspense fallback={<LoadingSpinner />}>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/home" component={Homepage} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signin/demo" component={DemoUserSignIn} />
+            <Route component={Error404} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     ),
     []
