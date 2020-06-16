e499f37c9ead53b986760aeaaaa90e590a891d0e
Lavi Sharma <lavisharma796@gmail.com>
2020-06-16 13:14:26 -0700

    move the new link to the list component

--- client/src/Components/Tickets/Tickets.js
+++ client/src/Components/Tickets/Tickets.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import AllTickets from './AllTickets';
+import useDocumentTitle from '../../hooks/useDocumentTitle';
 
 const Tickets = () => {
-  const { push } = useHistory();
+  useDocumentTitle('Tickets');
 
   return (
     <Container>
       <h3 className="display-3">These are tickets</h3>
-      <Button
-        onClick={() => push('/home/tickets/new')}
-        variant="success"
-        style={{ display: 'flex' }}
-      >
-        New Ticket
-        <i className="ml-2 gg-add-r" />
-      </Button>
-      <div style={{ marginBottom: '20px' }} />
       <AllTickets />
     </Container>
   );
