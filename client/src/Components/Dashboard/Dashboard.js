85e8fbc2e0bb58179f82d369149ac2365f950276
Lavi Sharma <lavisharma796@gmail.com>
2020-06-22 21:55:29 +0530

    new chart

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Helmet } from 'react-helmet';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import TicketsByPriority from './TicketsByPriority';
-import TicketsByType from './TicketsByType';
-import TicketByMonth from './TicketByMonth';
-import RecentTickets from './RecentTickets';
+import LoadingSpinner from '../../utils/LoadingSpinner';
+
+const TicketsByPriority = lazy(() => import('./TicketsByPriority'));
+const TicketsByType = lazy(() => import('./TicketsByType'));
+const TicketByMonth = lazy(() => import('./TicketByMonth'));
+const TicketByStatus = lazy(() => import('./TicketByStatus'));
+const RecentTickets = lazy(() => import('./RecentTickets'));
 
 const Dashboard = () => {
   return (
@@ -16,22 +19,24 @@ const Dashboard = () => {
         <meta name="title" content="Dashboard" />
         <meta name="description" content="Bug Tracker App dashboard" />
       </Helmet>
-      <Row>
-        <Col xs={6}>
-          <TicketsByPriority />
-        </Col>
-        <Col xs={6}>
-          <TicketsByType />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={6}>
-          <TicketByMonth />
-        </Col>
-        <Col xs={6}>
-          <RecentTickets />
-        </Col>
-      </Row>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Row>
+          <Col xs={6}>
+            <TicketsByPriority />
+          </Col>
+          <Col xs={6}>
+            <TicketsByType />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <TicketByStatus />
+          </Col>
+          <Col xs={6}>
+            <TicketByMonth />
+          </Col>
+        </Row>
+      </Suspense>
     </Container>
   );
 };
--- client/src/Components/Dashboard/TicketByStatus.js
+++ client/src/Components/Dashboard/TicketByStatus.js
@@ -3,10 +3,9 @@ import { PieChart, Pie, Tooltip, ResponsiveContainer, Cell } from 'recharts';
 import useGet from '../../hooks/useGet';
 import BeatSpinner from '../../utils/BeatSpinner';
 
-const TicketsByType = () => {
-  const { isLoading, response, error } = useGet('tickets/chart/type');
-
-  const colors = ['#10ac84', '#ff6b6b', '#2e86de', '#01a3a4'];
+const TicketByStatus = () => {
+  const { isLoading, error, response } = useGet('/tickets/chart/status');
+  const colors = ['#ff6348', '#5352ed'];
 
   if (isLoading) return <BeatSpinner />;
   else if (error) {
@@ -14,18 +13,13 @@ const TicketsByType = () => {
     return <p>There was some error</p>;
   } else {
     const { data } = response;
+    console.log(data);
     return (
       <>
-        <h5 className="text-center">Ticket types</h5>
+        <h5 className="text-center">Ticket Status</h5>
         <ResponsiveContainer width="100%" height={250}>
           <PieChart>
-            <Pie
-              dataKey="count"
-              data={data}
-              innerRadius={50}
-              outerRadius={100}
-              nameKey="type"
-            >
+            <Pie dataKey="count" data={data} nameKey="status">
               {data.map((_, i) => (
                 <Cell key={i} fill={colors[i % colors.length]} />
               ))}
@@ -38,4 +32,4 @@ const TicketsByType = () => {
   }
 };
 
-export default TicketsByType;
+export default TicketByStatus;
