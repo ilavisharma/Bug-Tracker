d5f3f3b8b414845f8761dd5d120f4e85d296d055
Lavi Sharma <lavisharma796@gmail.com>
2020-06-25 11:57:24 +0530

    use connection string as env variable because heroku rotates db credentials periodically

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,24 +1,9 @@
-const config = require('./config');
-const { database, host, password, port, user } = config;
-
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host,
-    user,
-    password,
-    database,
-    port,
-    ssl: true
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
 });
-
-// knex
-//   .raw('SELECT NOW()')
-//   .then(() => console.log('Connected to DB'))
-//   .catch(err => {
-//     console.log(err);
-//     throw err;
-//   });
 
 module.exports = knex;
